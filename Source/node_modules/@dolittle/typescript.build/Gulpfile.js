42307b9f26f38cb4d3c60174b03e7411d4bccb6b
Sindre Wilting <sindrewilting@gmail.com>
2020-06-11 14:58:49 +0200

    initial

---
+++ Source/node_modules/@dolittle/typescript.build/Gulpfile.js
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+let setup = require('./Distribution/Gulp/setup').default;
+setup(exports);
