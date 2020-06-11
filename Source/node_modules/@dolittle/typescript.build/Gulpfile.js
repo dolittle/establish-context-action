d986b5e53084941e355f92427dbdbc31af9da9a8
Sindre Wilting <sindrewilting@gmail.com>
2020-06-11 15:04:54 +0200

    remove node_modules

--- Source/node_modules/@dolittle/typescript.build/Gulpfile.js
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-let setup = require('./Distribution/Gulp/setup').default;
-setup(exports);
