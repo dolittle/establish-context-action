7e763f9a3372ed73d8f7cdbde6724a759e44a9e5
Einar Ingebrigtsen <einar@dolittle.com>
2020-10-26 10:24:52 +0100

    Converting to absolute path

--- Source/Version/VersionFromFileVersionFinder.ts
+++ Source/Version/VersionFromFileVersionFinder.ts
@@ -5,6 +5,7 @@ import { SemVer } from 'semver';
 import { IFindCurrentVersion } from './IFindCurrentVersion';
 import { ILogger } from '@dolittle/github-actions.shared.logging';
 import * as fs from 'fs';
+import * as path from 'path';
 
 /**
  * Represents an implementation of {@link IFindCurrentVersion} that reads version information from a file.
@@ -21,17 +22,22 @@ export class VersionFromFileVersionFinder implements IFindCurrentVersion {
     async find(prereleaseBranch: SemVer | undefined): Promise<SemVer> {
         const defaultVersion = new SemVer('1.0.0');
         try {
-            if (fs.existsSync(this._file)) {
-                const content = await fs.promises.readFile(this._file);
+            const absolutePath = path.resolve(this._file);
+
+            if (fs.existsSync(absolutePath)) {
+                const content = await fs.promises.readFile(absolutePath);
                 const contentAsString = content.toString();
                 const versionInfo = JSON.parse(contentAsString);
                 this._logger.info(`Version from file: ${versionInfo.version}`);
                 return new SemVer(versionInfo.version);
             } else {
+                this._logger.info('Version file does not exist - returning default version');
                 return defaultVersion;
             }
         } catch (e) {
-            return defaultVersion;        }
+            this._logger.info(`Problems getting version from file '${e}'`);
+            return defaultVersion;
+        }
     }
 
 }
