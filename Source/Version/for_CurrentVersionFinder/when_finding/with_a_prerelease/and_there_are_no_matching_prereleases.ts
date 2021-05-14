7ed9ae79ce83c0f8a2b1fa6c61c38547f95f18ae
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-05-14 20:18:56 +0200

    Write some specs that work

---
+++ Source/Version/for_CurrentVersionFinder/when_finding/with_a_prerelease/and_there_are_no_matching_prereleases.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { SemVer } from 'semver';
+import { a_version_finder } from '../../given/a_version_finder';
+
+describe('when finding with a prerelease and there are no matching prereleases', () => {
+    const finder = a_version_finder.with_sorted_versions('1.2.0', '1.2.0-alpha', '1.0.0', '1.0.0-beta.1', '1.0.0-beta', '1.0.0-alpha', '0.9.0');
+    const prerelease = new SemVer('1.1.0-alpha');
+
+    const result = finder.find(prerelease).then(_ => _.format());
+
+    it('should return the specified prerelease', () => result.should.eventually.equal('1.1.0-alpha'));
+});
