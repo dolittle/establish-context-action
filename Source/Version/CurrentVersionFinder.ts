28f2a6ea6c41f88203fe46bcaeed8c78be972e7e
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-05-14 23:41:16 +0200

    Fix CurrentVersionFinder

--- Source/Version/CurrentVersionFinder.ts
+++ Source/Version/CurrentVersionFinder.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import semver, { SemVer } from 'semver';
+import { SemVer } from 'semver';
 import { ILogger } from '@dolittle/github-actions.shared.logging';
 import { IFindCurrentVersion } from './IFindCurrentVersion';
 import { IVersionSorter } from './IVersionSorter';
@@ -32,44 +32,37 @@ export class CurrentVersionFinder implements IFindCurrentVersion {
      */
     async find(prereleaseBranch: SemVer | undefined): Promise<SemVer> {
         const versions = await this._versionFetcher.fetchPreviouslyReleasedVersions();
-        if (!versions || versions.length === 0) {
-            const defaultVersion = new SemVer('0.0.0');
+        const sorted = this._versionSorter.sort(versions, true);
+
+        this._logger.debug(`All version tags: [\n${sorted.join(',\n')}\n]`);
+
+        const filtered = this._filterApplicableVersions(sorted, prereleaseBranch);
+        this._logger.debug(`Filtered version tags: [\n${filtered.join(',\n')}\n]`);
+
+        if (filtered.length === 0) {
+            const defaultVersion = this._getDefaultVersion(prereleaseBranch);
             this._logger.info(`No version tags. Defaulting to version ${defaultVersion.version}`);
             return defaultVersion;
         }
 
-        this._logger.debug(`Version tags: [\n${versions.join(',\n')}\n]`);
-
-        const currentVersion = this._findGreatestMatchingVersion(this._versionSorter.sort(versions, true), prereleaseBranch);
+        const currentVersion = filtered[0];
         this._logger.info(`Current version '${currentVersion}'`);
         return currentVersion;
     }
 
-    private _findGreatestMatchingVersion(versionsDescending: SemVer[], prereleaseBranch: SemVer | undefined) {
+    private _filterApplicableVersions(versions: SemVer[], prereleaseBranch: SemVer | undefined) {
         if (prereleaseBranch === undefined) {
-            const greatestVersion = versionsDescending[0];
-            this._logger.debug(`Not searching for prerelease. Returning greatest version ${greatestVersion}`);
-            return greatestVersion;
+            this._logger.debug('Filtering only non-prerelease versions');
+            return versions.filter(_ => _.prerelease.length === 0);
+        } else {
+            this._logger.debug(`Filtering only versions matching prerelease ${prereleaseBranch}`);
+            return versions.filter(_ => _.compareMain(prereleaseBranch) === 0);
         }
+    }
 
-        this._logger.debug(`Searching for version with the greatest build number of prerelease ${prereleaseBranch}`);
-        for (const version of versionsDescending) {
-            this._logger.debug(`Checking version ${version}`);
-            if (semver.gt(prereleaseBranch, version)) {
-                this._logger.debug(`${prereleaseBranch} is greater than ${version}. Defaulting to ${prereleaseBranch}`);
-                return prereleaseBranch;
-            }
-            const versionPrerelease = version.prerelease;
-            if (versionPrerelease === null || versionPrerelease.length === 0)
-            {
-                this._logger.debug(`${version} is not a prerelease version. Skipping`);
-                continue;
-            }
-            if (prereleaseBranch.compareMain(version) === 0) {
-                this._logger.debug(`${prereleaseBranch} and ${version} match`);
-                return version;
-            }
-        }
-        return prereleaseBranch;
+    private _getDefaultVersion(prereleaseBranch: SemVer | undefined): SemVer {
+        return prereleaseBranch === undefined
+            ? new SemVer('0.0.0')
+            : prereleaseBranch;
     }
 }
